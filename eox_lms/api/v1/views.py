37c7c78865f2181c7482292abce6bfb32921fd05
John Stupples <l.stupples@uq.edu.au>
2022-07-07 12:33:51 +1000

    Removed import

--- eox_lms/api/v1/views.py
+++ eox_lms/api/v1/views.py
@@ -10,7 +10,7 @@ import logging
 import edx_api_doc_tools as apidocs
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
-from django.utils import six
+#from django.utils import six
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.exceptions import APIException, NotFound, ValidationError
